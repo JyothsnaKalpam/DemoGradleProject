264c55d48c3938e684256fb63b43ee0b714fc60c
jyoth <jyoth@192.168.1.107>
2022-09-27 17:06:22 +0530

    DemoGradle-New Dummy Class

---
+++ src/test/java/DemoGradle/Dummy.java
@@ -0,0 +1,17 @@
+package DemoGradle;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Dummy {
+	WebDriver driver;	
+	@Test
+	public void basic() {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/Drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+	}
+
+}
